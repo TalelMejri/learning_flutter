881dd60f92095db26579c5e7e2f9928c7989d5d5
talel <talelmejri8@gmail.com>
2023-08-01 15:26:48 +0100

    make style for text

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Center(
-      child: Text("Talel Mejri"),
+      child: Text(
+        "Talel Mejri",
+        style: TextStyle(color: Colors.blue, decoration: TextDecoration.none),
+      ),
     ),
     debugShowCheckedModeBanner: false,
   ));
