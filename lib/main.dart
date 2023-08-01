6e20649b213523b7451bdb4b9a6aa4334ab56ea2
talel <talelmejri8@gmail.com>
2023-08-01 16:45:57 +0100

    add image ,center text

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 
+//Exercie 1
+
+// void main() {
+//   runApp(MaterialApp(
+//     home: Center(
+//       child: Text(
+//         "Talel Mejri",
+//         style: TextStyle(color: Colors.blue, decoration: TextDecoration.none),
+//       ),
+//     ),
+//     debugShowCheckedModeBanner: false,
+//   ));
+// }
+
+//Exercice 2
 void main() {
-  runApp(MaterialApp(
-    home: Center(
-      child: Text(
-        "Talel Mejri",
-        style: TextStyle(color: Colors.blue, decoration: TextDecoration.none),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: Text("I AM RICH", style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.black,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/profile.png'),
+            width: 600,
+            height: 400,
+          ),
+        ),
       ),
     ),
-    debugShowCheckedModeBanner: false,
-  ));
+  );
 }
