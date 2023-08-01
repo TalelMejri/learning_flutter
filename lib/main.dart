f5d8c3ab534f26e2f888463d0512a07dbec7859f
talel <talelmejri8@gmail.com>
2023-08-01 15:10:19 +0100

    fix bugs

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:  Center(
-      child:  Text("Talel Mejri"),
+    home: Center(
+      child: Text("Talel Mejri"),
     ),
     debugShowCheckedModeBanner: false,
   ));
